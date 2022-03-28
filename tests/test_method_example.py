4c7b9f8e9dc9b96e8358458c1c913ee102f06345
Patrick Ingraham <pingraham@lsst.org>
2022-03-28 08:48:26 -0700

    add a test

--- tests/test_method_example.py
+++ tests/test_method_example.py
@@ -9,7 +9,15 @@ logger.level=logging.DEBUG
 
 class TestModel(unittest.TestCase):
     def test_example_method(self):
+        """ Check that proper value is returned."""
         multiplier=11
         value=10
+        expected_result=110
         logger.debug(f'Multiplier is {multiplier}, value is {value}')
-        self.assertTrue(example_method(value, multiplier=multiplier))
+        self.assertTrue(example_method(value, multiplier=multiplier), expected_result)
+
+    def test_input_error(self):
+        """Test that error handling raises an error when a string is declared."""
+        value = '10'  # strings are not supported
+        with self.assertRaises(IOError):
+            await example_method(value)
